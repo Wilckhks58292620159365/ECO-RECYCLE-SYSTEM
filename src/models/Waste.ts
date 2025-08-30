33a5844cc009acfc59f1165ec88c2033e826809c
Ahmed Ghalwash <128639447+Wilckhks58292620159365@users.noreply.github.com>
2025-08-30 16:06:06 +0300

    Update Waste.ts

--- src/models/Waste.ts
+++ src/models/Waste.ts
@@ -1,5 +1,5 @@
 import { Table, Column, Model, DataType, ForeignKey, BelongsTo } from 'sequelize-typescript';
-import { User } from './User';
+import User  from './User';
 
 @Table({ tableName: 'wastes', timestamps: true })
 export class Waste extends Model {
